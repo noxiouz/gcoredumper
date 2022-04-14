5165ec1579fff74168b1d0604d5a43e549a9cb25
Anton Tiurin <noxiouz@yandex.ru>
2022-04-14 22:35:30 +0100

    Add buildId
    Signed-off-by: Anton Tiurin <noxiouz@yandex.ru>

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"syscall"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/spf13/afero"
 
 	"github.com/noxiouz/gcoredumper/core"
-	"github.com/noxiouz/gcoredumper/dumper"
 	"github.com/noxiouz/gcoredumper/report"
 )
 
@@ -30,24 +28,27 @@ var (
 	configPath     = flag.String("cfg", "./config.textproto", "path to config")
 )
 
-func skipCoredump() bool {
-	return false
+func SetUpLogger(w io.Writer) {
+	log.SetOutput(w)
+	log.SetPrefix(fmt.Sprintf("%v: ", uuid.NewString()))
 }
 
 func main() {
 	flag.Parse()
+	reporter := report.New()
+	sink := &report.LogBasedReporter{Logger: log.Default()}
+	defer reporter.Report(sink)
+
 	// TODO(noxiouz): make configurable
 	f, err := os.OpenFile("/var/log/gcoredumper.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatal(err)
+		SetUpLogger(io.Discard)
+	} else {
+		SetUpLogger(f)
+		defer f.Close()
 	}
-	defer f.Close()
-	log.SetOutput(f)
-	log.SetPrefix(fmt.Sprintf("%v: ", uuid.NewString()))
-	reporter := report.New()
-	sink := &report.LogBasedReporter{Logger: log.Default()}
-	ctx := report.WithReport(context.Background(), reporter)
 
+	ctx := report.WithReport(context.Background(), reporter)
 	// TODO: add required arguments check
 	si := core.SystemInput{
 		// Pathname of Executable
@@ -66,26 +67,12 @@ func main() {
 		DumpTime: time.Unix(*timestampInSec, 0),
 		// Signal
 		Signal: syscall.Signal(*signalNum),
+		// Input
+		Stream: os.Stdin,
 	}
 
-	config := core.LocalConfig{
-		Dumper: &dumper.Configuraion{
-			Compression:      dumper.Configuraion_PLANE,
-			MaxDiskUsagePrct: 99,
-		},
-	}
-	reporter.AddInt("signal", int64(*signalNum))
-
-	pi, err := core.NewProcessInfo(ctx, si.InitialPid, si.NsPid, si.InitialTid, si.NsTid, afero.NewOsFs())
+	err = core.Run(ctx, si)
 	if err != nil {
-		log.Fatalf("faile to create ProcessInfo: %v", err)
+		reporter.AddError("core.run.error", err)
 	}
-	// TODO(noxiouz): respect get dumpable and core limit.
-	if !skipCoredump() {
-		if _, err := dumper.New(afero.NewOsFs()).Dump(ctx, os.Stdin, filepath.Join("/tmp", pi.CorefileName()), config.Dumper); err != nil {
-			log.Fatalf("failed to dump core %v", err)
-		}
-	}
-
-	reporter.Report(sink)
 }
