b0bdef096ab7d3dc7e38b2f66465c7f4cdd71e94
Anton Tiurin <atiurin@proton.me>
2022-04-18 18:16:41 +0100

    switch Report to on using protobuf

--- report/logbased.go
+++ report/logbased.go
@@ -2,29 +2,20 @@ package report
 
 import (
 	"log"
-	"time"
 )
 
 type LogBasedReporter struct {
 	*log.Logger
 }
 
-func (l *LogBasedReporter) ReportString(key string, value string) error {
-	log.Printf("%s = %s", key, value)
-	return nil
-}
-
-func (l *LogBasedReporter) ReportInt(key string, value int64) error {
-	log.Printf("%s = %d", key, value)
-	return nil
-}
-
-func (l *LogBasedReporter) ReportError(key string, err error) error {
-	log.Printf("%s = %v", key, err)
-	return nil
-}
-
-func (l *LogBasedReporter) ReportDuration(key string, d time.Duration) error {
-	log.Printf("%s = %v", key, d)
-	return nil
+func (l *LogBasedReporter) Log(record *Record) {
+	key := record.Name
+	switch value := record.Value.(type) {
+	case *Record_Str:
+		log.Printf("%s = %s", key, value)
+	case *Record_Number:
+		log.Printf("%s = %d", key, value)
+	case *Record_Duration:
+		log.Printf("%s = %v", key, value)
+	}
 }
